3a455391c279e8d377007eb8594196c0ea70776e
Future Computer Training Institute <107338637+fctiadmin@users.noreply.github.com>
2023-05-29 18:40:39 +0600

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,53 @@
+//ternary operator
+// var mark=70;
+// var result=mark>=80? "A+":"F";
+// document.write(result);
+
+
+//switch js
+// var alphabet=prompt("Enter Your Letter:");
+// switch(alphabet){
+	// case 'A':
+	// case 'a':
+	// case 'E':
+	// case 'e':
+	// case 'I':
+	// case 'i':
+	// case 'O':
+	// case 'o':
+	// case 'U':
+	// case 'u':
+	// document.write(alphabet +" is Vowel");
+	// break;
+	// case 'B':
+	// case 'b':
+	// case 'C':
+		// document.write(alphabet+' is Consonant');
+	// break;
+	// default :
+	// alert(alphabet +' is not letter!');
+
+	
+// }
+
+// for loop ............
+// for(var i=1;i<=100;i++){
+	// document.write(i);
+// }
+
+//while loop
+// var i=0;
+// while(i<=100){
+	
+	// document.write('<br/>'+i);
+	
+	// i++;
+// }
+
+//many number adding with loop........
+// let total=0;
+// for(var i=1;i<=5;i++){
+	 // total+=i;
+// }
+// document.write(total);
+
